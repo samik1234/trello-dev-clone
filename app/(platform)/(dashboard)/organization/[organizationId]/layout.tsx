906276bbeed673be4c2dc01812e89bad25d92676
samiksha <samikshawankhade11@gmail.com>
2023-12-19 22:13:12 +0530

    first commit

---
+++ app/(platform)/(dashboard)/organization/[organizationId]/layout.tsx
@@ -0,0 +1,31 @@
+import { OrgControl } from "./_components/org-control";
+import { auth } from "@clerk/nextjs";
+import { startCase } from "lodash";
+
+
+export async function generateMetadata() {
+    const { orgSlug } = auth();
+  
+    return {
+      title: startCase(orgSlug || "organization"),
+    };
+};
+
+
+
+const OrganizationIdLayout = ({
+    children
+}: {
+  children: React.ReactNode;   
+}) => {
+    return (
+        <>
+         <OrgControl />
+         {children}
+        </>
+           
+    )
+}
+
+
+export default OrganizationIdLayout;
